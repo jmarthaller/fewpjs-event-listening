a617633dd8bafa0eec4efddf2d226b21abc2fc63
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-16 15:20:43 -0500

    commit tree 40ecdb6ec3e6c6145b66d9c23538f01d4b43e09a

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+// // function addingEventListener() {
+// //     const input = document.getElementById('input');
+// // input.addEventListener('click', function(event) {
+// //   alert('I was clicked!');
+// // });
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.querySelector('#input')
+
 // function addingEventListener() {
-//     const input = document.getElementById('input');
-// input.addEventListener('click', function(event) {
-//   alert('I was clicked!');
-// });
+//   input.addEventListener('click', function(e) {
+//     console.log('I caught you on camera')
+// })
 // }
 
 
@@ -19,26 +41,33 @@
 
 
 
-const input = document.querySelector('#input')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function addingEventListener() {
-  input.addEventListener('click', function(e) {
-    console.log('I caught you on camera')
+
+
+
+const targetedInput = document.querySelector('#input')
+
+targetedInput.addEventListener('click', function(e) {
+  console.log(e.target)
 })
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
